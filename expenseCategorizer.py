38c033e9b6d54c28ac4a36cd44cf198a2ac44d4d
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-04-11 22:37:06 +0300

    update

--- expenseCategorizer.py
+++ expenseCategorizer.py
@@ -19,7 +19,7 @@ while True:
         print("Ensure your input value is an integer number!!!")
 
 # Store the list of items
-itemList = defaultdict(list)
+itemList = []
 currentIndex  = 0
 
 print("\nEnter the items in the format (e.g orange 50) where 'orange' is the item and '50' is the cost:")
@@ -37,30 +37,30 @@ while  currentIndex < noItems:
                 value = float(parts[1]) #treats as float
             except:
                 value = parts[1] ##treats as otherwise
+        itemList.append((key, value))  # Store (key, value) tuple
+
     else:
         print("Ensure you enter data using the correct format...\n (e.g orange 50) where 'orange' is the item and '50' is the cost")
     
-    itemList[key] = value
-
     currentIndex = currentIndex + 1
 
 
 # Classifying the user item list
 currentIndex = 0
 unavailableListItem = defaultdict(list)
-for item in itemList:
-    if str(item) == "food":
-        food[item].append(itemList[item])
-    elif str(item) == "transport":
-        transport[item].append(itemList[item])
-    elif str(item) == "bill":
-        bills[item].append(itemList[item])
-    elif str(item) == "book":
-        books[item].append(itemList[item])
-    elif str(item) == "entertainment":
-        entertainment[item].append(itemList[item])
+for key, item in itemList:
+    if key == "food":
+        food[key].append(item)
+    elif key == "transport":
+        transport[key].append(item)
+    elif key == "bill":
+        bills[key].append(item)
+    elif key == "book":
+        books[key].append(item)
+    elif key == "entertainment":
+        entertainment[key].append(item)
     else:
-        unavailableListItem[item].append(itemList[item])
+        unavailableListItem[key].append(item)
 
 
 #displaying the result for the categoried expenses
@@ -68,50 +68,49 @@ print("\nThe categorized expenses of the user include:")
 
 #considering the food  
 print("For Food:")
-if len(food) > 0:
-    for foodItem in food:
-        print(foodItem, food[foodItem])
+if food:
+ for foodItem, foodValues in food.items():
+        print(foodItem, foodValues)
 else:
     print("No food was recieved...")
 
-# #considering the transportation
-# print("For transportation:")    
-# if len(transport) > 0:
-#     for transportItem in transport:
-#         print(transportItem, transport[transportItem])
-# else:
-#     print("No transportation Item was recorded...")
+#considering the transportation
+print("For transportation:")    
+if transport:
+    for transportItem, transport in transport.items():
+        print(transportItem, transport)
+else:
+    print("No transportation Item was recorded...")
 
-# #considering the bills
-# print("For bills:")   
-# if len(bills) > 0:
-#     for bill in bills:
-#         print(bill, bills[bill])
-# else:
-#     print("No bills were recorded...")
+#considering the bills
+print("For bills:")   
+if bills:
+    for bill, bills in bills.items():
+        print(bill, bills)
+else:
+    print("No bills were recorded...")
 
-# #considering the entertainment
-# print("For entertainment:")   
-# if len(entertainment) > 0:
-#     for ent in entertainment:
-#         print(ent, entertainment[ent])
-# else:
-#     print("No entertaiment Item was recorded...")
+#considering the entertainment
+print("For entertainment:")   
+if entertainment:
+    for ent, entertainment in entertainment.items():
+        print(ent, entertainment)
+else:
+    print("No entertaiment Item was recorded...")
 
-# #considering the books  
-# print("For books:") 
-# if len(books) > 0:
-#     for book in books:
-#         print(book, books[book])
-# else:
-#     print("No books were recorded...")
+#considering the books  
+print("For books:") 
+if books:
+    for book, books in books.items():
+        print(book, books)
+else:
+    print("No books were recorded...")
 
-# #considering the unavailable items in our list
-# print("For Items we don't have in out category:") 
-# if len(unavailableListItem) > 0:
-#     for item in unavailableListItem:
-#         print(item, unavailableListItem[item])
+#considering the unavailable items in our list
+print("For Items we don't have in out category:") 
+if unavailableListItem:
+    for item, unavailableListItem in unavailableListItem.items():
+        print(item, unavailableListItem)
 
-# print("\n\nFinished processing the details...")
+print("\n\nFinished processing the details...")
 
-print(food)

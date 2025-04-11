3592ab6e2820f1899fa9d9c7b0be3054e2f582ce
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-04-11 22:28:39 +0300

    update

--- expenseCategorizer.py
+++ expenseCategorizer.py
@@ -1,10 +1,12 @@
+from collections import defaultdict
+
 # assuming these are the available list of items we can compare with
 # "food", "transport", "bills", "books", "entertainment"
-food  = {}
-entertainment = {} 
-transport = {}
-bills = {}
-books = {}
+food  = defaultdict(list)
+entertainment = defaultdict(list) 
+transport = defaultdict(list)
+bills = defaultdict(list)
+books = defaultdict(list)
 
 
 # Get the number of items
@@ -17,7 +19,7 @@ while True:
         print("Ensure your input value is an integer number!!!")
 
 # Store the list of items
-itemList = {}
+itemList = defaultdict(list)
 currentIndex  = 0
 
 print("\nEnter the items in the format (e.g orange 50) where 'orange' is the item and '50' is the cost:")
@@ -45,71 +47,71 @@ while  currentIndex < noItems:
 
 # Classifying the user item list
 currentIndex = 0
-unavailableListItem = {}
+unavailableListItem = defaultdict(list)
 for item in itemList:
     if str(item) == "food":
-        food[item] = itemList[item]
+        food[item].append(itemList[item])
     elif str(item) == "transport":
-        transport[item] = itemList[item]
+        transport[item].append(itemList[item])
     elif str(item) == "bill":
-        bills[item] = itemList[item]
+        bills[item].append(itemList[item])
     elif str(item) == "book":
-        books[item] = itemList[item]
+        books[item].append(itemList[item])
     elif str(item) == "entertainment":
-        entertainment[item] = itemList[item]
+        entertainment[item].append(itemList[item])
     else:
-        unavailableListItem[item] = itemList[item]
+        unavailableListItem[item].append(itemList[item])
 
 
 #displaying the result for the categoried expenses
 print("\nThe categorized expenses of the user include:")
-while True:
-    #considering the food  
-    print("For Food:")
-    if len(food) > 0:
-        for foodItem in food:
-            print(foodItem, food[foodItem])
-    else:
-        print("No food was recieved...")
-  
-  #considering the transportation
-    print("For transportation:")    
-    if len(transport) > 0:
-        for transportItem in transport:
-            print(transportItem, transport[transportItem])
-    else:
-        print("No transportation Item was recorded...")
- 
- #considering the bills
-    print("For bills:")   
-    if len(bills) > 0:
-        for bill in bills:
-            print(bill, bills[bill])
-    else:
-        print("No bills were recorded...")
-    
- #considering the entertainment
-    print("For entertainment:")   
-    if len(entertainment) > 0:
-        for ent in entertainment:
-            print(ent, entertainment[ent])
-    else:
-        print("No entertaiment Item was recorded...")
-    
- #considering the books  
-    print("For books:") 
-    if len(books) > 0:
-        for book in books:
-            print(book, books[book])
-    else:
-        print("No books were recorded...")
 
-#considering the unavailable items in our list
-    print("For Items we don't have in out category:") 
-    if len(unavailableListItem) > 0:
-        for item in unavailableListItem:
-            print(item, unavailableListItem[item])
+#considering the food  
+print("For Food:")
+if len(food) > 0:
+    for foodItem in food:
+        print(foodItem, food[foodItem])
+else:
+    print("No food was recieved...")
 
-    print("\n\nFinished processing the details...")
-    break
-    
+# #considering the transportation
+# print("For transportation:")    
+# if len(transport) > 0:
+#     for transportItem in transport:
+#         print(transportItem, transport[transportItem])
+# else:
+#     print("No transportation Item was recorded...")
+
+# #considering the bills
+# print("For bills:")   
+# if len(bills) > 0:
+#     for bill in bills:
+#         print(bill, bills[bill])
+# else:
+#     print("No bills were recorded...")
+
+# #considering the entertainment
+# print("For entertainment:")   
+# if len(entertainment) > 0:
+#     for ent in entertainment:
+#         print(ent, entertainment[ent])
+# else:
+#     print("No entertaiment Item was recorded...")
+
+# #considering the books  
+# print("For books:") 
+# if len(books) > 0:
+#     for book in books:
+#         print(book, books[book])
+# else:
+#     print("No books were recorded...")
+
+# #considering the unavailable items in our list
+# print("For Items we don't have in out category:") 
+# if len(unavailableListItem) > 0:
+#     for item in unavailableListItem:
+#         print(item, unavailableListItem[item])
+
+# print("\n\nFinished processing the details...")
+
+print(food)
